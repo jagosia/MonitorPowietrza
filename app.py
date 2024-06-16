30ebb6bd54031115e1d1cb8786cf736522713bbf
Małgorzata Jagieła <malgorzata.jagiela@outlook.com>
2024-06-16 18:09:31 +0200

    Some styling and message validation

--- app.py
+++ app.py
@@ -290,18 +290,18 @@ def register_post():
 
 
 class RegistrationForm(FlaskForm):
-    name = StringField('Imię', validators=[DataRequired(), Length(min=2, max=50)])
-    lastname = StringField('Nazwisko', validators=[DataRequired(), Length(min=2, max=50)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    phone = StringField('Telefon', validators=[DataRequired(), Length(min=9, max=15)])
-    city = StringField('Miasto', validators=[DataRequired(), Length(min=2, max=50)])
+    name = StringField('Imię', validators=[DataRequired(message="To pole jest wymagane"), Length(min=2, max=50, message="Wymagana długość od 2 do 50 znaków")])
+    lastname = StringField('Nazwisko', validators=[DataRequired(message="To pole jest wymagane"), Length(min=2, max=50, message="Wymagana długość od 2 do 50 znaków")])
+    email = StringField('Email', validators=[DataRequired(message="To pole jest wymagane"), Email()])
+    phone = StringField('Telefon', validators=[DataRequired(message="To pole jest wymagane"), Length(min=9, max=15, message="Wymagana długość od 2 do 15 znaków")])
+    city = StringField('Miasto', validators=[DataRequired(message="To pole jest wymagane"), Length(min=2, max=50, message="Wymagana długość od 2 do 50 znaków")])
 
     password = PasswordField('Hasło', validators=[
-        DataRequired(),
+        DataRequired(message="To pole jest wymagane"),
         Length(min=6, message='Hasło musi mieć przynajmniej 6 znaków.')
     ])
     password2 = PasswordField('Powtórz hasło', validators=[
-        DataRequired(),
+        DataRequired(message="To pole jest wymagane"),
         EqualTo('password', message='Hasła muszą być identyczne.')
     ])
 
